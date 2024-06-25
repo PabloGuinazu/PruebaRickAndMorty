1032091efdfb50a6bcc597506acb6dee5af4df7d
Pablo <guinazuzu@gmail.com>
2024-06-25 13:06:27 -0400

    se crean condiciones para que aparezcan iconos dependiendo del dato optenido por el usuario, esto con el, fin de hacer más reconocible al leer los datos, se añadieron varios componente jsx para los icon

--- src/Card.jsx
+++ src/Card.jsx
@@ -1,4 +1,12 @@
 import './Card.css';
+import RedIcon from './assets/RedIcon';
+import Greenicon from './assets/GreenIcon';
+import Unknown from './assets/Unknown';
+import AlienIcon from './assets/AlienIcon';
+import HumanIcon from './assets/HumanIcon';
+import MaleIcon from './assets/MaleIcon';
+import FemaleIcon from './assets/FemaleIcon';
+//se importan icon
 //se importan estilos y el componente recibe componente de app.jsx despues se construye la card con los estilos y datos correspondientes
 const Card =({ name, status,species,gender,image }) =>{
 	return(
@@ -6,11 +14,18 @@ const Card =({ name, status,species,gender,image }) =>{
 			<picture>
 				<img src={image} alt={name} />
 			</picture>
-			<div className=''>
 				<h2>{name}</h2>
-				<p>Status: {status}</p>
-				<p>Species: {species}</p>
-				<p>Gender: {gender}</p>
+			<div className='content'>
+				{/* se añaden condiciones dependiewndo del dato obtenido en el primer caso si esta vivo saldra el icon verde, sino el rojo todo esto atra vez de componentes jsx */}
+				<p>Status: {status}{'  '}
+					{status === 'Dead' ? <RedIcon/> :
+					 status === 'Alive' ? <Greenicon/> : <Unknown/> }</p>
+				<p>Species: {species}{'  '}
+						{species === 'Alien' ? <AlienIcon/> : <HumanIcon/>}
+				</p>
+				<p>Gender: {gender}{'  '}
+				{gender === 'Male' ? <MaleIcon/> :
+					 gender === 'Female' ? <FemaleIcon/> : <Unknown/> }</p>
 			</div>
 		</div>
 	);
